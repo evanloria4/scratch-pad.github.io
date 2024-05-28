6f6389256827d69c6f1d52f9041d9465c47ceed8
evanloria4 <evan.loria4@gmail.com>
2024-05-27 20:27:23 -0500

    ScratchPad Complete

--- day-2/homework/fizz-buzz.js
+++ day-2/homework/fizz-buzz.js
@@ -10,8 +10,29 @@
  *  c. AND for the multiples of five print “Buzz”.
  *  d. FINALLY, for numbers which are multiples of both three and five print “FizzBuzz”.
 */
+/* 
+I: n/a
+O: Numbers from 1 - 100
+C: Prints fizz for multiples of 3, buzz for multiples of 5, 
+prints fizzbuzz if number is both
+E: n/a
+*/
 function fizzBuzz() {
     // YOUR CODE GOES BELOW HERE //
+// Use loop to print numbers
+for (var i = 1; i <= 100; i ++){
+    if (i % 3 === 0 && i % 5 === 0){
+        console.log('FizzBuzz');
+    } else if(i % 3 === 0){
+        console.log('Fizz');
+    } else if(i % 5 === 0){
+        console.log('Buzz');
+    } else {
+        console.log(i)
+    }
+    }
+    return i;
+    
 
 
 
